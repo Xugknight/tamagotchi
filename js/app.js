af3dd184ffd55f3b987ba829aa6da21884f5970c
timothyn <tnguyen_42@yahoo.com>
2026-02-05 20:39:31 -0500

    stopping point

--- js/app.js
+++ js/app.js
@@ -9,8 +9,16 @@ let gameOver; // boolean for win/loss state
 
 
 /*------------------------ Cached Element References ------------------------*/
+const boredomStatEl = document.querySelector('#boredom-stat');
+const hungerStatEl = document.querySelector('#hunger-stat');
+const sleepinessStatEl = document.querySelector('#sleepiness-stat');
 
+const playBtnEl = document.querySelector('#play');
+const feedBtnEl = document.querySelector('#feed');
+const sleepBtnEl = document.querySelector('#sleep');
 
+const gameMessageEl = document.querySelector('#message');
+const resetBtnEl = document.querySelector('#restart');
 
 /*-------------------------------- Functions --------------------------------*/
 
