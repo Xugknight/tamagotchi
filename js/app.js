9c401d4e202e310dd527b3dd5844bb487f21d165
timothyn <tnguyen_42@yahoo.com>
2026-02-09 13:55:07 -0500

    add init,runGame,render, and updateState. stats increasing randomly working

--- js/app.js
+++ js/app.js
@@ -21,8 +21,34 @@ const gameMessageEl = document.querySelector('#message');
 const resetBtnEl = document.querySelector('#restart');
 
 /*-------------------------------- Functions --------------------------------*/
+init();
 
+function init() {
+    state = {
+        boredom: 0,
+        hunger: 0,
+        sleepiness: 0,
+    };
+    gameOver = false;
+    timer = setInterval(runGame, 2000);
+};
 
+function runGame() {
+    updateStates();
+    console.log(state);
+    render();
+};
+
+function render() { 
+    
+};  
+
+function updateStates() {
+    state.boredom += Math.floor(Math.random() * 4);
+    state.hunger += Math.floor(Math.random() * 4);
+    state.sleepiness += Math.floor(Math.random() * 4);
+};
+    
 
 /*----------------------------- Event Listeners -----------------------------*/
 
